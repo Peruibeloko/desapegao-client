137e637fc840fbe095c565a3841072f1ad61540b
peruibeloko <cmendonca32@gmail.com>
2024-02-24 11:33:15 -0300

    update workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false }
+  devtools: { enabled: false },
+  vite: {
+    build: {
+      outDir: 'docs'
+    }
+  }
 })
