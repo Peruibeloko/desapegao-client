766828079d709c924ca0611ba3481c3945528f50
peruibeloko <cmendonca32@gmail.com>
2024-03-04 18:53:50 -0300

    connect frontend

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>desapega desapega olx</h1>
+  <h2>{{ data }}</h2>
+</template>
+
+<script setup lang="ts">
+const data = await fetch('https://desapegao.deno.dev/listing').then(res => res.json())
+console.log(data)
+</script>
