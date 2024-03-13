e6a814b9c2f841b3b86f70c10c058b212ca8ca32
Peruibeloko <cmendonca32@gmail.com>
2024-03-13 12:31:43 +0000

    Deploying to gh-pages from @ Peruibeloko/desapegao-client@9b27591e070f8f48cf9b87f76c37b352d6858fcd 🚀

---
+++ _nuxt/finish.q084l0A6.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./_plugin-vue_export-helper.DlAUqK2U.js";const r={};function c(e,t){return null}const s=n(r,[["render",c]]);export{s as default};
---
+++ _nuxt/review.Cax4_K7V.js
@@ -0,0 +1,1 @@
+import{d as e,v as t,t as o}from"./entry.CFu36IEj.js";const n={type:"button"},a=e({__name:"review",setup(r){return(c,s)=>(o(),t("button",n,"Tudo certo!"))}});export{a as default};
